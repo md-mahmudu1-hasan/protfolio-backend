736f991958e9c3a70fa044fc8829c558559a1f6d
md-mahmudu1-hasan <mdmahmudulhasan0008@gmail.com>
2026-01-06 12:29:51 +0600

    deply

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const database = client.db("profolio_data");
     const projects = database.collection("projects");
 
@@ -59,7 +59,7 @@ async function run() {
       res.send(project);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
